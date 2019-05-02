888f5a1e347f5d3ef5cb6f557c1ce7546634cbdd
kim dongseok(hpc) <pop9846@gmail.com>
2019-05-02 23:08:25 +0900

    사진 추가 작동 확인
    사진 추가 작동 확인

--- bf4_states.py
+++ bf4_states.py
@@ -1,5 +1,7 @@
 import requests
 from tkinter import *
+from PIL import Image, ImageTk
+import os
 
 data=requests.get("https://api.bf4stats.com/api/playerInfo?plat=pc&name=sharqia&output=json").json() #서버에 GET 요청
 print(data)
@@ -13,14 +15,18 @@ print(rank)
 
 window = Tk()
 window.title("전장4")
+originimage1=Image.open("image/ranks/r140.png").resize((100,100))
+rankImage=ImageTk.PhotoImage(originimage1)
 width = 500
 height = 300
 canvas = Canvas(window, width=width, height=height)
 canvas.pack()
 Label(canvas,text="name="+player['name']).pack()
+Label(window, image=rankImage).pack()
 Label(canvas,text="level="+str(rank)).pack()
 Label(canvas,text="skill="+str(state['skill'])).pack()
 Label(canvas,text="kills="+str(state['kills'])).pack()
 Label(canvas,text="deaths="+str(state['deaths'])).pack()
+os.system("D:/GitHub/ScriptProject2019/bg-video.webm")
 
 window.mainloop()
